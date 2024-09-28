7b076cddedd2c49d281501ad827c7d08349777e6
japp47 <jappreets4747@gmail.com>
2024-09-28 15:56:24 +0530

    Undo Redo tracker added

--- canvas.js
+++ canvas.js
@@ -2,24 +2,27 @@ let canvas = document.querySelector('canvas')
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let lineWidth = document.querySelector('.pencil-width')
-let lineColor = document.querySelectorAll('.pencil-color')
-let eraserWidth = document.querySelector('.eraser-width')
+let pencilColor = document.querySelectorAll(".pencil-color");
+let pencilWidthElem = document.querySelector(".pencil-width");
+let eraserWidthElem = document.querySelector(".eraser-width");
 let clearCanvasButton = document.querySelector('.clear-canvas');
 let download = document.querySelector('.download');
+let undo = document.querySelector('.undo')
+let redo = document.querySelector('.redo');
 let penColor = "red"
 let eraserColor = "white"
-let penWidth = lineWidth.value;
-let eraserwide = eraserWidth.value;
+let penWidth = pencilWidthElem.value;
+let eraserWidth = eraserWidthElem.value;
 
-
+let mousedown = false;
 let tool = canvas.getContext("2d");
 
 tool.strokeStyle = penColor;
 tool.lineWidth = penWidth;
-tool.eraserWidth = eraserwide;
 
-let mousedown = false;
+let undoredoTrack = [];
+let track = 0;
+
 
 canvas.addEventListener("mousedown", (e) => {
     mousedown = true;
@@ -36,48 +39,82 @@ canvas.addEventListener("mousemove", (e) => {
         drawStroke({
             x: e.clientX,
             y: e.clientY,
-            // color: eraserFlag? eraserColor:penColor,
-            // width: eraserWidth? eraserwide:penWidth
+            color: eraserFlag ? eraserColor : penColor,
+            width: eraserFlag ? eraserWidth : penWidth
         })
     }
 });
 
 canvas.addEventListener("mouseup", (e) => {
     mousedown = false;
+    let url = canvas.toDataURL();
+    undoredoTrack.push(url);
+    track = undoredoTrack.length-1;
 })
+
+undo.addEventListener("click", (e) => {
+    if(track > 0) {
+        track--;
+    }
+    let trackObject = {
+        trackValue: track,
+        undoredoTrack
+    }
+    undoRedoCanvas(trackObject);
+})
+redo.addEventListener("click", (e) => {
+    if(track < undoredoTrack.length-1) {
+        track++;
+    }
+    let trackObject = {
+        trackValue: track,
+        undoredoTrack
+    }
+    undoRedoCanvas(trackObject);
+});
+function undoRedoCanvas(trackObject) {
+    track = trackObject.trackValue;
+    undoredoTrack = trackObject.undoredoTrack
+
+    let url = undoredoTrack[track]
+    let img = new Image();
+    img.src = url;
+    img.onload = (e) => {
+        tool.drawImage(img, 0, 0, canvas.width, canvas.height);
+    }
+}
 function beginPath(strokeObject) {
     tool.beginPath();
     tool.moveTo(strokeObject.x, strokeObject.y);
 }
 function drawStroke(strokeObject) {
-    // tool.strokeStyle = strokeObject.color;
-    // tool.lineWidth = strokeObject.width;
+    tool.strokeStyle = strokeObject.color;
+    tool.lineWidth = strokeObject.width;
     tool.lineTo(strokeObject.x, strokeObject.y);
     tool.stroke();
 }
-lineColor.forEach((colorElem) => {
+pencilColor.forEach((colorElem) => {
     colorElem.addEventListener("click", (e) => {
         let color = colorElem.classList[0];
         penColor = color;
-        tool.strokeStyle = penColor
+        tool.strokeStyle = penColor;
     })
 })
 
-lineWidth.addEventListener('change', (e) => {
-    penWidth =  lineWidth.value
- tool.lineWidth = penWidth;
+pencilWidthElem.addEventListener("change", (e) => {
+    penWidth = pencilWidthElem.value;
+    tool.lineWidth = penWidth;
 })
-
-eraserWidth.addEventListener('change', (e) => {
-    eraserwide =  eraserWidth.value
-    tool.lineWidth = eraserwide;
+eraserWidthElem.addEventListener("change", (e) => {
+    eraserWidth = eraserWidthElem.value;
+    tool.lineWidth = eraserWidth;
 })
 
 eraser.addEventListener("click", (e) => {
-    if(eraserFlag) {
-        tool.strokeStyle = eraserColor
-        tool.lineWidth = eraserwide;
-    } else{
+    if (eraserFlag) {
+        tool.strokeStyle = eraserColor;
+        tool.lineWidth = eraserWidth;
+    } else {
         tool.strokeStyle = penColor;
         tool.lineWidth = penWidth;
     }
